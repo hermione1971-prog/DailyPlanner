32aed86de2d32732fb0366358acac354e9786626
hermione1971-prog <276454837+hermione1971-prog@users.noreply.github.com>
2026-05-15 17:50:39 +0900

    Move planner files to site root

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-const cacheName = "mainichi-techo-public-v1";
-const assets = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+const cacheName = "mainichi-totonou-v2";
+const assets = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon.svg",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png",
+  "./anne-study.jpg",
+];
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
@@ -8,12 +19,21 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys()
+    caches
+      .keys()
       .then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))))
       .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(cacheName).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
